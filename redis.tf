9458772d8310f5db61143f2ac3746fc64d3d9d30
KalluruS <>
2021-01-22 08:46:12 +0530

    Update terraform-project/terraform-databases/

---
+++ redis.tf
@@ -0,0 +1,49 @@
+resource "aws_elasticache_subnet_group" "redis" {
+  name       = "redis-${var.ENV}"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
+
+  tags = {
+    Name = "Redis Cluster subnet group"
+  }
+}
+
+resource "aws_elasticache_parameter_group" "default" {
+  name   = "redis-pg-${var.ENV}"
+  family = "redis5.0"
+}
+
+resource "aws_security_group" "allow-redis" {
+  name                    = "allow-redis-${var.ENV}"
+  description             = "allow-redis-${var.ENV}"
+  vpc_id                  = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description           = "SSH"
+    from_port             = 6379
+    to_port               = 6379
+    protocol              = "tcp"
+    cidr_blocks             = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port             = 0
+    to_port               = 0
+    protocol              = "-1"
+    cidr_blocks           = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name                  = "allow-redis-${var.ENV}"
+  }
+}
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id                = "redis-${var.ENV}"
+  engine                    = "redis"
+  node_type                 = "cache.t3.small"
+  num_cache_nodes           = 1
+  parameter_group_name      = aws_elasticache_parameter_group.default.name
+  engine_version            = "5.0.5"
+  port                      = 6379
+  subnet_group_name         = aws_elasticache_subnet_group.redis.name
+}
