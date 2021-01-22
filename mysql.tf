81b1415f56f03eeb17593c36f9e388f003366b98
KalluruS <>
2021-01-22 08:01:32 +0530

    Update terraform-project/terraform-databases/

--- mysql.tf
+++ mysql.tf
@@ -7,10 +7,12 @@ resource "aws_db_subnet_group" "mysql" {
   }
 }
 
-resource "aws_db_parameter_group" "mysql" {
-  name   = "mysql-pg"
-  family = "mysql5.7"
+resource "aws_rds_cluster_parameter_group" "mysql" {
+  name        = "mysql-cluster-pg-${var.ENV}"
+  family      = "aurora5.7"
+  description = "RDS default cluster parameter group"
 }
+
 
 resource "aws_rds_cluster" "mysql" {
   cluster_identifier                  = "mysql-${var.ENV}"
@@ -22,5 +24,6 @@ resource "aws_rds_cluster" "mysql" {
   master_password                     = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["MYSQL_PASS"]
   backup_retention_period             = 5
   preferred_backup_window             = "07:00-09:00"
-  db_cluster_parameter_group_name     = aws_db_parameter_group.mysql.name
+  db_cluster_parameter_group_name     = aws_rds_cluster_parameter_group.mysql.name
 }
+
